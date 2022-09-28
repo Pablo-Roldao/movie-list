185a1e5d3c8794bcdae5772fc1cd534c6274a45e
Pablo Santos <pablo.roldao.santos@gmail.com>
2022-09-28 15:58:22 -0300

    Aumentando responsividade do MovieCard...

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,9 +6,9 @@ const imagesURL = import.meta.env.VITE_IMG;
 
 const MovieCard = ({ movie, showLink = true }) => {
   return (
-    <div className="movie-card bg-black m-2 p-2 rounded text-center col-2" id="movie-card">
-      <img src={imagesURL + movie.poster_path} alt={movie.title}  className="img-fluid rounded" id="movie-card-img" />
-      <h3>{movie.title}</h3>
+    <div className="movie-card bg-black m-2 p-2 rounded text-center col" id="movie-card">
+      <img src={imagesURL + movie.poster_path} alt={movie.title}  className="img-fluid rounded" width="200px" id="movie-card-img" />
+      <h3 className="display-6">{movie.title}</h3>
       <p>
         <FaStar /> {movie.vote_average}
       </p>
